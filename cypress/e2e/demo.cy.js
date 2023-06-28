3fac414b590bd57b8912e192bd2c3b7f4b23f897
Felix P <felix@saucelabs.com>
2023-06-28 16:37:03 +0200

    Capture only viewport

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -3,12 +3,16 @@ context('Actions', () => {
     cy.visit('https://duckduckgo.com/?va=v&t=ha&q=Tacoma&iax=images&ia=images')
     cy.wait(10000);
 
-    cy.screenshot('02');
+    cy.screenshot('02', {
+      capture: 'viewport',
+    });
     cy.get('div > span > img').first().click();
     cy.wait(10000);
 
     cy.get('div > span > img').eq(2).click();
-    cy.screenshot('03');
+    cy.screenshot('03', {
+      capture: 'viewport',
+    });
     cy.wait(10000);
   });
 })
