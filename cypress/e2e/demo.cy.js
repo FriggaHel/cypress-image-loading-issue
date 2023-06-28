903f7afa273708c3ee057877b92a012c93090d52
Felix P <felix@saucelabs.com>
2023-06-28 16:29:20 +0200

    Fix delay

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,10 +1,10 @@
 context('Actions', () => {
   it('.visit', () => {
     cy.visit('https://duckduckgo.com/?va=v&t=ha&q=Tacoma&iax=images&ia=images')
-    cy.wait(60000);
+    cy.wait(10000);
 
     cy.screenshot('02');
-    cy.get('div > span > img').click();
+    cy.get('div > span > img').first().click();
     cy.wait(10000);
 
     cy.get('div > span > img').eq(2).click();
