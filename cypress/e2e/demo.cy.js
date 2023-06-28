be53130efd58bca741b7cf1b77d9016911a27b3a
Felix P <felix@saucelabs.com>
2023-06-28 16:40:50 +0200

    Remove pause

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,18 +1,15 @@
 context('Actions', () => {
   it('.visit', () => {
     cy.visit('https://duckduckgo.com/?va=v&t=ha&q=Tacoma&iax=images&ia=images')
-    cy.wait(10000);
 
     cy.screenshot('02', {
       capture: 'viewport',
     });
     cy.get('div > span > img').first().click();
-    cy.wait(10000);
 
     cy.get('div > span > img').eq(2).click();
     cy.screenshot('03', {
       capture: 'viewport',
     });
-    cy.wait(10000);
   });
 })
