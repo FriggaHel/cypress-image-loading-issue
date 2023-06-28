1cba43393180feb47cea65957a6b3039dcfc826e
Felix P <felix@saucelabs.com>
2023-06-28 16:24:33 +0200

    Duckduckgo

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,16 +1,14 @@
 context('Actions', () => {
   it('.visit', () => {
-    cy.visit('https://smartpath.myalexandertoyota.com/inventory?dealerCd=02037&source=t3&zipcode=85365&type=new')
+    cy.visit('https://duckduckgo.com/?va=v&t=ha&q=Tacoma&iax=images&ia=images')
     cy.wait(60000);
 
     cy.screenshot('02');
-    cy.get('div.mat-form-field-infix').click();
+    cy.get('div > span > img').click();
     cy.wait(10000);
 
-    cy.get('#mat-option-3 > span > span').click();
+    cy.get('div > span > img').eq(2).click();
     cy.screenshot('03');
-    cy.get('app-card-container .vehicle-name').first().click().then(()=>{
-      cy.get('div .vehicle-image').first().click();
-    });
+    cy.wait(10000);
   });
 })
