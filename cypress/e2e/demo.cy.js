570cb3b016a38d9abc0b8e695611a31faf9870ba
Felix P <felix@saucelabs.com>
2023-06-28 16:41:26 +0200

    Capture viewport only

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -3,12 +3,12 @@ context('Actions', () => {
     cy.visit('https://smartpath.myalexandertoyota.com/inventory?dealerCd=02037&source=t3&zipcode=85365&type=new')
     cy.wait(60000);
 
-    cy.screenshot('02');
+    cy.screenshot('02', { capture: 'viewport' });
     cy.get('div.mat-form-field-infix').click();
     cy.wait(10000);
 
     cy.get('#mat-option-3 > span > span').click();
-    cy.screenshot('03');
+    cy.screenshot('03', { capture: 'viewport' });
     cy.get('app-card-container .vehicle-name').first().click().then(()=>{
       cy.get('div .vehicle-image').first().click();
     });
