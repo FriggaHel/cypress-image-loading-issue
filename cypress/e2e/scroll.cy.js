18ae415c625572f980e5e527d8b0dfa7015ff951
Felix P <felix@saucelabs.com>
2023-06-29 13:04:53 +0200

    add scroll demo

---
+++ cypress/e2e/scroll.cy.js
@@ -0,0 +1,26 @@
+context('Actions', () => {
+  it('.visit', () => {
+    cy.visit('https://smartpath.myalexandertoyota.com/inventory?dealerCd=02037&source=t3&zipcode=85365&type=new')
+    cy.wait(10000);
+
+    cy.scrollTo(0, 500);
+    cy.wait(10000);
+
+    cy.scrollTo(0, 1000);
+    cy.wait(10000);
+
+    cy.get('div.mat-form-field-infix').click();
+    cy.wait(10000);
+
+    cy.get('#mat-option-3 > span > span').click();
+    cy.wait(10000);
+
+    cy.screenshot('03', { capture: 'fullPage' });
+    cy.wait(10000);
+
+    cy.get('app-card-container .vehicle-name').first().click().then(()=>{
+      cy.get('div .vehicle-image').first().click();
+    });
+    cy.wait(10000);
+  });
+})
